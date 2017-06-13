fe6eb0244b99b38b2188c2bedfa17ae0ad21fdc6
이재민 <jaeminstar@gmail.com>
2017-06-13 16:27:40 +0900

    DB 2.0 버전에 맞게 수정(쿼리문, 로그인)

--- src/main/java/kr/co/nextdoor/member/dto/MemberDTO.java
+++ src/main/java/kr/co/nextdoor/member/dto/MemberDTO.java
@@ -6,30 +6,31 @@ public class MemberDTO {
 	private String name;
 	
 	public MemberDTO(){}
-	
+
 	public String getMember_id() {
 		return member_id;
 	}
+
 	public void setMember_id(String member_id) {
 		this.member_id = member_id;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	@Override
-	public String toString() {
-		return "MemberDTO [member_id=" + member_id + ", password=" + password + ", name=" + name + "]";
-	}
-	
+
 	
 }

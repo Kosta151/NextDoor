290185ea6b43c7d9ef6e845d0e86e9951c66a389
pcs <pcs0331@naver.com>
2017-06-15 10:37:01 +0900

    ChatConfig 무시 오류 해결

--- src/main/java/kr/co/nextdoor/chat/dao/a.java
+++
@@ -1,5 +0,0 @@
-package kr.co.nextdoor.chat.dao;
-
-public class a {
-
-}
---
+++ src/main/java/kr/co/nextdoor/chat/service/ChatConfig.java
@@ -0,0 +1,37 @@
+
+package kr.co.nextdoor.chat.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.PerConnectionWebSocketHandler;
+
+@Configuration
+@EnableWebMvc
+@EnableWebSocket
+@ComponentScan(basePackages = { "kr.co.nextdoor.chat" })
+public class ChatConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+	
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		
+		registry.addHandler(chatWebSocketHandler(), "/chat.htm").addInterceptors(new ChatHandlerInterceptor())
+				.withSockJS();
+	}
+
+	@Bean
+	public WebSocketHandler chatWebSocketHandler() {
+		return new PerConnectionWebSocketHandler(ChatHandler.class);
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}

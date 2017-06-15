59f95208ef6c774d401c57d0a351cdddb4fa9d93
최성용(kosta151) <csyong92@naver.com>
2017-06-15 18:00:37 +0900

    calendar-conf-events.js 와 fullcalendarTask 만 수정

--- src/main/webapp/WEB-INF/views/fullcalendar/fullcalendarTask.jsp
+++ src/main/webapp/WEB-INF/views/fullcalendar/fullcalendarTask.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,7 +11,7 @@
 <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>ㄴ
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <title>Insert title here</title>
 <script type="text/javascript">
    
@@ -46,32 +47,37 @@
       </section>
       <!-- /MAIN CONTENT -->
       
-						<!-- Modal -->
-                      	  <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal2" class="modal fade">
-                          <div class="modal-dialog">
-                          <form action="insertProject.htm" method="post">
+					  <!-- Modal -->
+                      <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal2" class="modal fade">
+                          <div class="modal-dialog modal-sm">
                               <div class="modal-content">
                                   <div class="modal-header">
                                       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                                       <h4 class="modal-title">새 프로젝트 생성</h4>
                                   </div>
+                                  <form action="insertProject.htm" method="post">
                                   <div class="modal-body">
-                                      <p class="centered"><!-- <img class="img-circle" width="80" src="resources/main/assets/img/ui-sam.jpg"> --></p>
                                       <p>프로젝트명</p>
-                                      <input type="text" name="project_name" placeholder="project명을 입력해주세요." autocomplete="off" class="form-control placeholder-no-fix">
-                                      
-                                <input type="radio" name="public" value="public" >공개<br>
-                                <input type="radio" name="public" value="private">비공개<br>
+                                      	<input type="hidden" name="workspace_no" value="${workspace_no}">
+										<input type="text" name="project_name" placeholder="project명을 입력해주세요." autocomplete="off" class="form-control placeholder-no-fix">
+										<br>
+										<div class="btn-group" data-toggle="buttons">
+											<label class="btn btn-default">
+												<input type="radio" name="options" value="public" autocomplete="off" checked> 공개
+											</label>
+											<label class="btn btn-default">
+												<input type="radio" name="options" value="private" autocomplete="off"> 비공개
+											</label>
+										</div>
                                   </div>
                                   <div class="modal-footer centered">
-                                      <button class="btn btn-theme03" type="submit">생성</button>
-                                      <button data-dismiss="modal" class="btn btn-theme04" type="button">취소</button>
+                                      <button class="btn btn-warning" type="submit">생성</button>
+                                      <button data-dismiss="modal" class="btn btn-default" type="button">취소</button>
                                   </div>
+                                  </form>
                               </div>
-                            </form>
                           </div>
-                      </div>
-                      <!-- modal -->
+                      </div><!-- modal -->
 
 </body>
 </html>
--- src/main/webapp/resources/main/assets/js/calendar-conf-events.js
+++ src/main/webapp/resources/main/assets/js/calendar-conf-events.js
@@ -4,7 +4,7 @@ var Script = function () {
     /* initialize the external events
      -----------------------------------------------------------------*/
 
-    $('#external-events div.external-event').each(function() {
+  /*  $('#external-events div.external-event').each(function() {
 
         // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
         // it doesn't need to have a start or end
@@ -22,7 +22,7 @@ var Script = function () {
             revertDuration: 0  //  original position after the drag
         });
 
-    });
+    });*/
 
 
     /* initialize the calendar
@@ -33,12 +33,55 @@ var Script = function () {
     var m = date.getMonth();
     var y = date.getFullYear();
 
-    calendar = $('#calendar').fullCalendar({
-    	dayClick: function() {
+    	calendar = $('#calendar').fullCalendar({
+    	
+    		/*events: function(start, end, callback) {
+    			$.ajax({
+    			    url: '/myfeed.json',
+    			    dataType: 'json',
+    			    success: function(data, text, request) {
+    			    	var events = eval(data.jsonTxt);
+    			        callback(events);
+    			    }
+    			});
+    		},*/
+    	
+    	//캘린더 한글 적용
+    	header: {
+            left: "prev,next today",
+            center: "title",
+            right: 'month,agendaWeek,agendaDay'
+            },
+            titleFormat: {
+            month: "yyyy년 MMMM",
+            week: "[yyyy] MMM d일{ [yyyy] MMM d일}",
+            day: "yyyy년 MMM d일 dddd"
+            },
+            allDayDefault: true,
+            defaultView: "month",
+            editable: false,
+            weekends : false,
+            monthNames: ["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"],
+            monthNamesShort: ["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"],
+            dayNames: ["일요일","월요일","화요일","수요일","목요일","금요일","토요일"],
+            dayNamesShort: ["일","월","화","수","목","금","토"],
+            buttonText: {
+            today : "오늘",
+            month : "월별",
+            week : "주별",
+            day : "일별"},
+            
+            select : function(event, jsEvent, view){
+               $('#myModal2').modal();
+            },
+            /*dayClick: function() { //날짜를 클릭하면 모달창이
     		 $('#myModal2').modal();
-        },
-        /* editable: true,*/
-        /*droppable: true, */// this allows things to be dropped onto the calendar !!!
+            },*/
+            editable: true, //크기를 줄이거나 움직이는
+            selectable:true, //달력을 누르면 파란표시되는
+        /* droppable: true,*/ 
+        /// this allows things to be dropped onto the calendar !!!
+        
         /*drop: function(date, allDay) { // this function is called when something is dropped
 
             // retrieve the dropped element's stored Event Object
@@ -63,73 +106,26 @@ var Script = function () {
 
         },*/
        
-        /*events: [
+        events: [
             {
-                title: 'My Event',
+                title: '프로젝트명',
                 start: '2017-06-12',
-                url: 'http://google.com/'
+                end: '2017-06-22',
+                url: 'http:projectList.htm'
             }
             // other events here
         ],
-        eventClick: function(event) {
-            if (event.url) {
-                window.open(event.url);
-                return false;
-            }
-        },*/
+        
        /* eventClick: function(calEvent, jsEvent, view) {
-           $("#myModal").dialog('open')*/
-            /*alert('Event: ' + calEvent.title);
+            
+            alert('Event: ' + calEvent.title);
             alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
             alert('View: ' + view.name);
 
-            // change the border color just for fun
-            $(this).css('color', 'red');*/
-
-       /* },*/
-      header: {
-         left: "prev,next today",
-         center: "title",
-         right: 'month,agendaWeek,agendaDay'
-         },
-         titleFormat: {
-         month: "yyyy년 MMMM",
-         week: "[yyyy] MMM d일{ [yyyy] MMM d일}",
-         day: "yyyy년 MMM d일 dddd"
-         },
-         allDayDefault: true,
-         defaultView: "month",
-         editable: false,
-         weekends : false,
-         monthNames: ["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"],
-         monthNamesShort: ["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"],
-         dayNames: ["일요일","월요일","화요일","수요일","목요일","금요일","토요일"],
-         dayNamesShort: ["일","월","화","수","목","금","토"],
-         buttonText: {
-         today : "오늘",
-         month : "월별",
-         week : "주별",
-         day : "일별"
-         },
-         selectable:true
-         /*,
-         select : function(event, jsEvent, view){
-            $('#myModal2').modal();
-         }*/
-           
-
-
-            
-    });
-   /*$('#myModal2').dialog({
-        autoOpen:false,
-           modal : true,
-          draggable : true, //창 드래그 못하게
-          resizable : false, //창 크기 고정
-          height : 350,
-          width : 550,  
-         
-     });*/
+            $(this).css('color', 'red');
+        }*/
+ 		});
+   
    /*
    $('#deletemodalclose').click(function(){
        $('#myModalDelete').dialog('close'); 
@@ -139,13 +135,17 @@ var Script = function () {
        $('#myModal').dialog('close'); 
       
    });*/
-   $('#start').datepicker({
+   
+        
+   /* 
+    $('#start').datepicker({
        dateFormat: "yy-mm-dd"
     });
     $('#end').datepicker({
        dateFormat: "yy-mm-dd"
        
     });
+    */
    
 
 

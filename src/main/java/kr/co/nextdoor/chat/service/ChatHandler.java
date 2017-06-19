d6e0b699d85f757def33cc83f29029b03c4b8346
이재민 <jaeminstar@gmail.com>
2017-06-19 10:10:44 +0900

    채팅 수정

--- src/main/java/kr/co/nextdoor/chat/service/ChatHandler.java
+++ src/main/java/kr/co/nextdoor/chat/service/ChatHandler.java
@@ -25,6 +25,7 @@ public class ChatHandler extends TextWebSocketHandler {
 		connectedUsers = new ArrayList<WebSocketSession>();
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		Map<String, Object> map = session.getAttributes();
@@ -41,11 +42,7 @@ public class ChatHandler extends TextWebSocketHandler {
 			projectmember.put(project_no, conn);
 	}
 
-	@Override
-	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		connectedUsers.remove(session);
-	}
-
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		Map<String, Object> map = session.getAttributes();
@@ -53,15 +50,44 @@ public class ChatHandler extends TextWebSocketHandler {
 		String project_no = (String)map.get("project_no");
 		System.out.println("----------그룹 채팅방-------------");
 		System.out.println("user 이름 : "+user_id);
+		System.out.println("session id : " +session.getId());
 		System.out.println("Group 번호 : "+project_no);
 		System.out.println("메시지 : "+message.getPayload());		
 		System.out.println("------------------------------");
+		System.out.println();
 		List<WebSocketSession> conn = (List<WebSocketSession>) projectmember.get(project_no);
 		Map<String, Object> data = new HashMap<String, Object>();
 		data.put("user_id", user_id);
 		data.put("message", message.getPayload());
 		ObjectMapper om = new ObjectMapper();
 		 String jsonStr = om.writeValueAsString(data);
+		for(WebSocketSession webSocketSession : conn){
+			if(!session.getId().equals(webSocketSession)){
+				webSocketSession.sendMessage(new TextMessage(jsonStr));		
+			}
+		}
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		Map<String, Object> map = session.getAttributes();
+		String user_id = (String)map.get("user_id");
+		
+		String project_no = (String)map.get("project_no");
+		System.out.println(user_id + "님 접속종료");
+		List<WebSocketSession> conn = (List<WebSocketSession>) projectmember.get(project_no);
+		Map<String, Object> data = new HashMap<String, Object>();
+	
+		
+		conn.remove(session);
+		session.close();
+		projectmember.remove(project_no);
+		projectmember.put(project_no, conn);
+		
+		data.put("user_id", user_id);
+		data.put("message", "님이 접속을 종료하였습니다.");
+		ObjectMapper om = new ObjectMapper();
+		String jsonStr = om.writeValueAsString(data);
 		for(WebSocketSession webSocketSession : conn){
 			if(!session.getId().equals(webSocketSession)){
 				webSocketSession.sendMessage(new TextMessage(jsonStr));		
